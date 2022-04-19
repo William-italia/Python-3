e569a2426ea43226b12dcf6decf7113d653b449f
william-italia <williamitalia70@outlook.com>
2022-04-19 01:42:43 -0300

    5-5

---
+++ ComandoPrint/app.py
@@ -0,0 +1,21 @@
+'''
+print('William', 'Italia', 'Outra coisa')
+print(12345)
+print('Jorgin', 'do', 'grau', sep='-', end='-')
+print('e', 'Roberto', 'Carlos', sep='-', end='.')
+'''
+
+"""
+824.176.070-18
+"""
+
+print('\n-------------------------------------')
+print('824', '176', '070', sep='.', end='-18')
+print('\n-------------------------------------')
+print('824', '176', '070', sep='.', end='-')
+print('18')
+print('-------------------------------------')
+
+
+print('Estou', 'aprendendo', 'Python', sep='-', end=' ')
+print('isso é muito', 'legal', sep='-', end='')
