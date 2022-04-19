5a1e390a20c67203fab21a4ff27dce791f16d531
william-italia <williamitalia70@outlook.com>
2022-04-19 01:18:09 -0300

    Pythonnn
    Testando usar o python no vs

---
+++ comentarios/app.py
@@ -0,0 +1,11 @@
+# Print imprime uma mensagem
+print('Hello Word!') #comentario
+
+# print('Tenis, Denis, ...')
+
+""" Bloco de comentario
+print('Hello Word!')
+print('Hello Word!')
+print('Hello Word!')
+print('Hello Word!')
+"""
